4c7b778616c6dc01cbf3b2004c5e2e06d9282b9d
Bhavesh864 <bhaveshkhatri864@gmail.com>
2023-07-12 17:17:56 +0530

    bhavesh_dev

--- lib/pages/smallscreen_dashboard.dart
+++ lib/pages/smallscreen_dashboard.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yes_broker/TabScreens/account_screens/profile_screen.dart';
+
 import 'package:yes_broker/TabScreens/main_screens/chat_screen.dart';
 import 'package:yes_broker/TabScreens/main_screens/inventroy_screen.dart';
 import 'package:yes_broker/TabScreens/main_screens/lead_screen.dart';
 import 'package:yes_broker/constants/utils/colors.dart';
 import 'package:yes_broker/constants/utils/constants.dart';
-import 'package:yes_broker/controllers/menu_controller.dart';
 import 'package:yes_broker/widgets/app/app_bar.dart';
 import 'package:yes_broker/widgets/app/speed_dial_button.dart';
 import 'package:yes_broker/widgets/calendar_view.dart';
 import 'package:yes_broker/widgets/todo/todo_list_view.dart';
 
-class SmallScreen extends StatelessWidget {
-  final SideMenuController menuController = Get.put(SideMenuController());
+final currentIndexProvider = StateProvider<int>((ref) {
+  return 0;
+});
 
+class SmallScreen extends ConsumerWidget {
   SmallScreen({super.key});
 
   final List<Widget> _pages = [
@@ -29,39 +32,43 @@ class SmallScreen extends StatelessWidget {
     const InventoryScreen(),
     const LeadScreen(),
     const ChatScreen(),
+    const ProfileScreen(),
   ];
 
   // int menuController.selectedMobilePageIndex.value = 0;
 
   @override
-  Widget build(BuildContext context) {
-    return Obx(
-      () => Scaffold(
-        appBar: MobileAppBar(context, GlobalKey()),
-        bottomNavigationBar: BottomNavigationBar(
-          unselectedItemColor: const Color.fromARGB(255, 158, 153, 153),
-          selectedItemColor: AppColor.primary,
-          showUnselectedLabels: false,
-          currentIndex: menuController.selectedMobilePageIndex.value,
-          onTap: (value) => menuController.selectMobilePage(value),
-          backgroundColor: Colors.white,
-          items: List.generate(
-            bottomBarItems.length,
-            (index) => BottomNavigationBarItem(
-              icon: Icon(
-                bottomBarItems[index]["icon"],
-                color: index == menuController.selectedMobilePageIndex.value
-                    ? AppColor.primary
-                    : Colors.black,
-              ),
-              label: bottomBarItems[index]['title'],
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentIndex = ref.watch(currentIndexProvider);
+    return Scaffold(
+      appBar: MobileAppBar(context, (selectedVal) {
+        print(selectedVal);
+        if (selectedVal == 'Profile') {
+          ref.read(currentIndexProvider.notifier).update((state) => 3);
+        }
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: const Color.fromARGB(255, 158, 153, 153),
+        selectedItemColor: AppColor.primary,
+        showUnselectedLabels: false,
+        onTap: (value) =>
+            ref.read(currentIndexProvider.notifier).update((state) => value),
+        currentIndex: currentIndex,
+        backgroundColor: Colors.white,
+        items: List.generate(
+          bottomBarItems.length,
+          (index) => BottomNavigationBarItem(
+            icon: Icon(
+              bottomBarItems[index]["icon"],
+              color: index == currentIndex ? AppColor.primary : Colors.black,
             ),
+            label: bottomBarItems[index]['title'],
           ),
         ),
-        body: _pages[menuController.selectedMobilePageIndex.value],
-        floatingActionButton: CustomSpeedDialButton(),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
+      body: _pages[currentIndex],
+      floatingActionButton: const CustomSpeedDialButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
