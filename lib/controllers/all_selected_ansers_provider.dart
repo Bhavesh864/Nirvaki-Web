82e9bbab004e4bc9a3ecf57e09ac6bd45ac96840
manishchayal14 <manishchayal14@gmail.com>
2023-07-13 15:29:23 +0530

    manish

--- lib/controllers/all_selected_ansers_provider.dart
+++ lib/controllers/all_selected_ansers_provider.dart
@@ -1,4 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:random_string/random_string.dart';
+import 'package:yes_broker/constants/firebase/broker_info.dart';
+import 'package:yes_broker/constants/firebase/inventory_details.dart';
+
+import 'package:yes_broker/constants/firebase/user_info.dart';
 
 final allChipSelectedAnwersProvider =
     StateNotifierProvider<AllChipSelectedAnwers, List<Map<String, dynamic>>>(
@@ -18,8 +23,25 @@ class AllChipSelectedAnwers extends StateNotifier<List<Map<String, dynamic>>> {
     state.removeAt(index);
   }
 
-  void submitInventoryDetails() {
-    print('state $state');
+  final randomId = randomNumeric(5);
+  void submitInventoryDetails() async {
+    final String propertyCategory = getDataById(state, 1);
+    final String inventoryCategory = getDataById(state, 2);
+    final InventoryDetails inventory = InventoryDetails(
+        inventoryTitle: "$propertyCategory Apartment",
+        inventoryDescription: "inventoryDescription",
+        inventoryId: "IN$randomId",
+        inventoryStatus: "New",
+        brokerid: authentication.currentUser!.uid,
+        inventorycategory: inventoryCategory,
+        propertycategory: propertyCategory,
+        createdby: Createdby(
+            userfirstname: "bhavesh",
+            userid: authentication.currentUser!.uid,
+            userlastname: "khatri"));
+    await InventoryDetails.addInventoryDetails(inventory);
+    print('category $inventoryCategory');
+    print('type $propertyCategory');
   }
 }
 
