37a22ac07c3b49fbf247295a49a9ba76f9892f4e
Bhavesh864 <bhaveshkhatri864@gmail.com>
2023-07-26 17:26:20 +0530

    bhavesh_dev

--- lib/pages/add_inventory.dart
+++ lib/pages/add_inventory.dart
@@ -210,7 +210,9 @@ class _AddInventoryState extends ConsumerState<AddInventory> {
                           ),
                         )
                       : response == "success"
-                          ? const InventorySuccessWidget()
+                          ? const WorkItemSuccessWidget(
+                              isInventory: true,
+                            )
                           : const Center(
                               child: CircularProgressIndicator.adaptive(),
                             ),
