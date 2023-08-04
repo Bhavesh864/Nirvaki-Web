20d0a6cb36c2a5301fbc9fe43d08bf64b6a4dc1d
manishchayal14 <manishchayal14@gmail.com>
2023-08-04 18:26:52 +0530

    added

--- lib/pages/largescreen_dashboard.dart
+++ lib/pages/largescreen_dashboard.dart
@@ -154,129 +154,129 @@ void _showChatDialog(BuildContext context) {
   );
 }
 
-  // void _showChatDialog(BuildContext context) {
-  //   final RenderBox overlay = Overlay.of(context).context.findRenderObject() as RenderBox;
-  //   final chatButton = overlay.localToGlobal(Offset.zero);
+// void _showChatDialog(BuildContext context) {
+//   final RenderBox overlay = Overlay.of(context).context.findRenderObject() as RenderBox;
+//   final chatButton = overlay.localToGlobal(Offset.zero);
 
-  //   showMenu(
-  //     context: context,
-  //     position: RelativeRect.fromLTRB(
-  //       chatButton.dx,
-  //       chatButton.dy,
-  //       chatButton.dx + 1000, // Adjust this value to position the dialog horizontally
-  //       chatButton.dy + 300, // Adjust this value to position the dialog vertically
-  //     ),
-  //     items: [
-  //       PopupMenuItem(
-  //         child: Container(
-  //           width: 200,
-  //           child: Column(
-  //             mainAxisSize: MainAxisSize.min,
-  //             children: [
-  //               ListTile(
-  //                 leading: Icon(Icons.chat_outlined),
-  //                 title: Text('Chat Dialog Box'), // Replace with your dialog title
-  //                 subtitle: Text('This is the content of the chat dialog box.'), // Replace with your dialog content
-  //               ),
-  //               Align(
-  //                 alignment: Alignment.centerRight,
-  //                 child: IconButton(
-  //                   onPressed: () {
-  //                     Navigator.of(context).pop(); // Close the dialog box
-  //                   },
-  //                   icon: Icon(Icons.close),
-  //                 ),
-  //               ),
-  //             ],
-  //           ),
-  //         ),
-  //         enabled: false,
-  //       ),
-  //     ],
-  //   );
-  // }
+//   showMenu(
+//     context: context,
+//     position: RelativeRect.fromLTRB(
+//       chatButton.dx,
+//       chatButton.dy,
+//       chatButton.dx + 1000, // Adjust this value to position the dialog horizontally
+//       chatButton.dy + 300, // Adjust this value to position the dialog vertically
+//     ),
+//     items: [
+//       PopupMenuItem(
+//         child: Container(
+//           width: 200,
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               ListTile(
+//                 leading: Icon(Icons.chat_outlined),
+//                 title: Text('Chat Dialog Box'), // Replace with your dialog title
+//                 subtitle: Text('This is the content of the chat dialog box.'), // Replace with your dialog content
+//               ),
+//               Align(
+//                 alignment: Alignment.centerRight,
+//                 child: IconButton(
+//                   onPressed: () {
+//                     Navigator.of(context).pop(); // Close the dialog box
+//                   },
+//                   icon: Icon(Icons.close),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//         enabled: false,
+//       ),
+//     ],
+//   );
+// }
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final currentIndex = ref.watch(largeScreenTabsProvider);
+@override
+Widget build(BuildContext context, WidgetRef ref) {
+  final currentIndex = ref.watch(largeScreenTabsProvider);
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SingleChildScrollView(
-            child: Container(
-              color: Colors.white,
-              height: 600,
-              padding: EdgeInsets.only(top: height! * 0.2),
-              child: NavigationRail(
-                backgroundColor: Colors.white,
-                minWidth: 60,
-                useIndicator: false,
-                onDestinationSelected: (index) {
-                  ref.read(largeScreenTabsProvider.notifier).update((state) => index);
-                },
-                destinations: sideBarItems
-                    .sublist(0, 6)
-                    .map(
-                      (e) => NavigationRailDestination(
-                        icon: Icon(e.iconData),
-                        selectedIcon: Icon(e.iconData, color: AppColor.primary),
-                        label: const Text('Yes Broker'),
-                      ),
-                    )
-                    .toList(),
-                selectedIndex: currentIndex > 5 ? 0 : currentIndex,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 20,
-            child: Column(
-              children: [
-                LargeScreenNavBar((selectedVal) {
-                  if (selectedVal != 'Logout') {
-                    ref.read(largeScreenTabsProvider.notifier).update((state) => 6);
-                    final ProfileMenuItems profile = profileMenuItems.firstWhere((element) => element.title == selectedVal);
-                    ref.read(selectedItemProvider.notifier).setSelectedItem(profile);
-                  } else if (selectedVal == "Logout") {
-                    authentication.signOut().then((value) => {Navigator.of(context).pushReplacementNamed(AppRoutes.loginScreen)});
-                    UserHiveMethods.deleteData(authentication.currentUser?.uid);
-                  }
-                }),
-                Expanded(
-                  child: sideBarItems[currentIndex].screen,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          const CustomSpeedDialButton(),
-          const SizedBox(
-            height: 10,
-          ),
-          CircleAvatar(
-            radius: 28,
-            backgroundColor: AppColor.primary,
-            child: IconButton(
-              icon: const Icon(
-                Icons.chat_outlined,
-                color: Colors.white,
-                size: 24,
-              ),
-              onPressed: () {
-                popupMenuItem('title');
+  return Scaffold(
+    backgroundColor: Colors.white,
+    body: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            height: 600,
+            padding: EdgeInsets.only(top: height! * 0.2),
+            child: NavigationRail(
+              backgroundColor: Colors.white,
+              minWidth: 60,
+              useIndicator: false,
+              onDestinationSelected: (index) {
+                ref.read(largeScreenTabsProvider.notifier).update((state) => index);
               },
+              destinations: sideBarItems
+                  .sublist(0, 6)
+                  .map(
+                    (e) => NavigationRailDestination(
+                      icon: Icon(e.iconData),
+                      selectedIcon: Icon(e.iconData, color: AppColor.primary),
+                      label: const Text('Yes Broker'),
+                    ),
+                  )
+                  .toList(),
+              selectedIndex: currentIndex > 5 ? 0 : currentIndex,
             ),
           ),
-        ],
-      ),
-    );
-  }
+        ),
+        Expanded(
+          flex: 20,
+          child: Column(
+            children: [
+              LargeScreenNavBar((selectedVal) {
+                if (selectedVal != 'Logout') {
+                  ref.read(largeScreenTabsProvider.notifier).update((state) => 6);
+                  final ProfileMenuItems profile = profileMenuItems.firstWhere((element) => element.title == selectedVal);
+                  ref.read(selectedItemProvider.notifier).setSelectedItem(profile);
+                } else if (selectedVal == "Logout") {
+                  authentication.signOut().then((value) => {Navigator.of(context).pushReplacementNamed(AppRoutes.loginScreen)});
+                  UserHiveMethods.deleteData(authentication.currentUser?.uid);
+                }
+              }),
+              Expanded(
+                child: sideBarItems[currentIndex].screen,
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+    floatingActionButton: Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        const CustomSpeedDialButton(),
+        const SizedBox(
+          height: 10,
+        ),
+        CircleAvatar(
+          radius: 28,
+          backgroundColor: AppColor.primary,
+          child: IconButton(
+            icon: const Icon(
+              Icons.chat_outlined,
+              color: Colors.white,
+              size: 24,
+            ),
+            onPressed: () {
+              popupMenuItem('title');
+            },
+          ),
+        ),
+      ],
+    ),
+  );
 }
+
   // }
