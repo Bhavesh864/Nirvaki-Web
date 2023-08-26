39a053f5ca991baa17afc8d724c37b9c4a142cb2
madhavsewag04 <madhavsewag04@gmail.com>
2023-08-26 11:29:01 +0530

    chat changes

--- lib/widgets/chat/chat_input.dart
+++ lib/widgets/chat/chat_input.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class ChatInput extends StatefulWidget {
   const ChatInput({super.key, required this.onSendMessage});
