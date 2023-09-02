8a3ffc75a6ebf5661978268669fdd1b2c94d1db8
Bhavesh864 <bhaveshkhatri864@gmail.com>
2023-09-02 15:24:44 +0530

    bhavesh_dev

--- lib/widgets/chat/chat_input.dart
+++ lib/widgets/chat/chat_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 
 class ChatInput extends StatefulWidget {
   const ChatInput({super.key, required this.onSendMessage});
@@ -11,70 +12,113 @@ class ChatInput extends StatefulWidget {
 
 class _ChatInputState extends State<ChatInput> {
   final TextEditingController message = TextEditingController();
+  bool isShowEmojiContainer = false;
+  FocusNode focusNode = FocusNode();
+
+  void hideEmojiContainer() {
+    setState(() {
+      isShowEmojiContainer = false;
+    });
+  }
+
+  void showEmojiContainer() {
+    setState(() {
+      isShowEmojiContainer = true;
+    });
+  }
+
+  void showKeyboard() => focusNode.requestFocus();
+
+  void hideKeyboard() => focusNode.unfocus();
+
+  void toggleEmojiKeyboardContainer() {
+    if (isShowEmojiContainer) {
+      showKeyboard();
+      hideEmojiContainer();
+    } else {
+      hideKeyboard();
+      showEmojiContainer();
+    }
+  }
 
   void sendMessage() {
-    if (message.text.isEmpty) {
+    if (message.text.trim().isEmpty) {
       return;
     }
-    widget.onSendMessage(message.text);
-    message.text = '';
+    widget.onSendMessage(message.text.trim());
+    message.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          InkWell(
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: Icon(
-                Icons.mood,
-                size: 18,
+    return Column(
+      children: [
+        Container(
+          color: Colors.white,
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              InkWell(
+                onTap: toggleEmojiKeyboardContainer,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Icon(
+                    !isShowEmojiContainer ? Icons.mood : Icons.keyboard_outlined,
+                    size: 18,
+                  ),
+                ),
               ),
-            ),
-            onTap: () {},
-          ),
-          Expanded(
-            child: TextField(
-              controller: message,
-              minLines: 1,
-              maxLines: 7,
-              textCapitalization: TextCapitalization.sentences,
-              autocorrect: true,
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                focusedBorder: InputBorder.none,
-                hintText: 'Start typing...',
+              Expanded(
+                child: TextField(
+                  focusNode: focusNode,
+                  controller: message,
+                  minLines: 1,
+                  maxLines: 7,
+                  textCapitalization: TextCapitalization.sentences,
+                  autocorrect: true,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    hintText: 'Start typing...',
+                  ),
+                ),
               ),
-            ),
-          ),
-          InkWell(
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5.0),
-              child: Icon(
-                Icons.alternate_email,
-                size: 18,
+              InkWell(
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Icon(
+                    Icons.alternate_email,
+                    size: 18,
+                  ),
+                ),
+                onTap: () {},
               ),
-            ),
-            onTap: () {},
-          ),
-          InkWell(
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 12),
-              child: Icon(
-                Icons.send,
-                size: 18,
+              InkWell(
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12),
+                  child: Icon(
+                    Icons.send,
+                    size: 18,
+                  ),
+                ),
+                onTap: () {
+                  sendMessage();
+                },
               ),
-            ),
-            onTap: () {
-              sendMessage();
-            },
+            ],
           ),
-        ],
-      ),
+        ),
+        isShowEmojiContainer
+            ? SizedBox(
+                height: 310,
+                child: EmojiPicker(
+                  onEmojiSelected: (category, emoji) {
+                    message.text = message.text + emoji.emoji;
+                  },
+                ),
+              )
+            : const SizedBox(),
+      ],
     );
   }
 }
--- lib/widgets/chat/chat_users_list.dart
+++ lib/widgets/chat/chat_users_list.dart
@@ -1,122 +1,126 @@
 import 'package:flutter/material.dart';
+
 import 'package:yes_broker/Customs/text_utility.dart';
 import 'package:yes_broker/constants/firebase/userModel/user_info.dart';
 import 'package:yes_broker/constants/utils/colors.dart';
 import 'package:yes_broker/constants/utils/constants.dart';
-import 'package:yes_broker/constants/utils/image_constants.dart';
+import 'package:yes_broker/customs/responsive.dart';
 import 'package:yes_broker/screens/main_screens/chat_screen.dart';
 
 class ChatUserList extends StatelessWidget {
-  const ChatUserList({super.key, required this.users});
-
-  // ignore: prefer_typing_uninitialized_variables
+  final Function(int) onPressed;
   final List<User> users;
+  const ChatUserList({
+    super.key,
+    required this.users,
+    this.onPressed = _defaultOnTapFunction,
+  });
+
+  static void _defaultOnTapFunction(k) {}
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (ctx, index) => UserList(user: users[index]));
-  }
-}
-
-class UserList extends StatelessWidget {
-  const UserList({super.key, required this.user});
-
-  // ignore: prefer_typing_uninitialized_variables
-  // final user;
-  final User user;
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.white,
-      child: ListTile(
-        onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (ctx) => ChatScreen(
-                user: user,
-              ),
-            ),
-          );
-        },
-        splashColor: Colors.grey[350],
-        leading: Hero(
-          tag: user.userId,
-          child: CircleAvatar(
+    return ListView.separated(
+      separatorBuilder: (context, index) {
+        return const Divider();
+      },
+      itemCount: users.length,
+      itemBuilder: (ctx, index) {
+        return Material(
+          color: Colors.white,
+          child: ListTile(
+            minVerticalPadding: 0,
+            // contentPadding: EdgeInsets.zero,
+            dense: true,
+            onTap: () {
+              if (Responsive.isMobile(context)) {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => ChatScreen(
+                      user: users[index],
+                    ),
+                  ),
+                );
+              } else {
+                onPressed(index);
+              }
+            },
+            splashColor: Colors.grey[350],
+            leading: // Hero(
+                // tag: user.userId,
+                CircleAvatar(
               radius: 26,
-              backgroundImage:
-                  NetworkImage(user.image.isEmpty ? noImg : user.image)),
-        ),
-        title: Flexible(
-          child: AppText(
-            text: '${user.userfirstname} ${user.userlastname} ',
-            textColor: const Color.fromRGBO(44, 44, 46, 1),
-            fontWeight: FontWeight.w500,
-            fontsize: 16,
-          ),
-        ),
-        subtitle: const Row(
-          children: [
-            // ignore: unrelated_type_equality_checks
-            // if (user["seen"] == true)
-            //   const Padding(
-            //     padding: EdgeInsets.only(right: 5),
-            //     child: Icon(
-            //       Icons.done_all,
-            //       size: 20,
-            //       color: Color.fromRGBO(31, 162, 255, 1),
-            //     ),
-            //   ),
-            Flexible(
-              child: Padding(
-                padding: EdgeInsets.only(right: 10),
-                child: AppText(
-                  overflow: TextOverflow.ellipsis,
-                  text: 'Hey,',
-                  textColor: Color.fromRGBO(155, 155, 155, 1),
-                  fontWeight: FontWeight.w400,
-                  fontsize: 15,
-                  maxLines: 1,
-                ),
+              backgroundImage: NetworkImage(
+                users[index].image.isEmpty ? noImg : users[index].image,
               ),
             ),
-          ],
-        ),
-        trailing: Column(
-          children: [
-            const AppText(
-              text: '11:20 am',
-              textColor: Color.fromRGBO(155, 155, 155, 1),
-              fontWeight: FontWeight.w400,
-              fontsize: 12,
-              maxLines: 1,
+            // ),
+            title: AppText(
+              text: '${users[index].userfirstname} ${users[index].userlastname} ',
+              textColor: const Color.fromRGBO(44, 44, 46, 1),
+              fontWeight: FontWeight.w500,
+              fontsize: 16,
             ),
-            // ignore: unrelated_type_equality_checks
-            if (true)
-              Container(
-                margin: const EdgeInsets.only(top: 5),
-                width: 20,
-                height: 20,
-                decoration: const BoxDecoration(
-                  color: AppColor.primary,
-                  shape: BoxShape.circle,
-                ),
-                child: const Center(
-                  child: Text(
-                    '2',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 11,
-                      fontWeight: FontWeight.bold,
+            subtitle: const Row(
+              children: [
+                // if (user["seen"] == true)
+                //   const Padding(
+                //     padding: EdgeInsets.only(right: 5),
+                //     child: Icon(
+                //       Icons.done_all,
+                //       size: 20,
+                //       color: Color.fromRGBO(31, 162, 255, 1),
+                //     ),
+                //   ),
+                Flexible(
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: AppText(
+                      overflow: TextOverflow.ellipsis,
+                      text: 'Hey,',
+                      textColor: Color.fromRGBO(155, 155, 155, 1),
+                      fontWeight: FontWeight.w400,
+                      fontsize: 15,
+                      maxLines: 1,
                     ),
                   ),
                 ),
-              ),
-          ],
-        ),
-      ),
+              ],
+            ),
+            trailing: Column(
+              // mainAxisSize: MainAxisSize.min,
+              children: [
+                const AppText(
+                  text: '11:20 am',
+                  textColor: Color.fromRGBO(155, 155, 155, 1),
+                  fontWeight: FontWeight.w400,
+                  fontsize: 10,
+                  maxLines: 1,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  width: 20,
+                  height: 20,
+                  decoration: const BoxDecoration(
+                    color: AppColor.primary,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      '2',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 11,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
