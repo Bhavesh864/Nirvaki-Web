9b27ffd76d4b88186b2d0ca67f6ae14b3b0789f7
Bhavesh864 <bhaveshkhatri864@gmail.com>
2023-09-14 14:28:31 +0530

    push

--- lib/screens/main_screens/caledar_screen.dart
+++ lib/screens/main_screens/caledar_screen.dart
@@ -1,117 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:yes_broker/constants/utils/colors.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
 
   @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  late DateTime _displayedMonth;
+
+  @override
+  void initState() {
+    super.initState();
+    _displayedMonth = DateTime.now();
+  }
+
+  void _previousMonth() {
+    setState(() {
+      _displayedMonth = DateTime(_displayedMonth.year, _displayedMonth.month - 1, 1);
+    });
+  }
+
+  void _nextMonth() {
+    setState(() {
+      _displayedMonth = DateTime(_displayedMonth.year, _displayedMonth.month + 1, 1);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Calendar Screen'),
+    return Row(
+      children: [
+        Expanded(
+          flex: 3,
+          child: Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+            child: Center(
+              child: SfCalendar(
+                headerHeight: 0,
+                view: CalendarView.week,
+                timeSlotViewSettings: const TimeSlotViewSettings(startHour: 9, endHour: 18),
+                showTodayButton: true,
+                showNavigationArrow: true,
+                backgroundColor: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: AppColor.primary,
+                    ),
+                    onPressed: _previousMonth,
+                  ),
+                  Text(
+                    DateFormat.yMMMM().format(_displayedMonth),
+                    style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: AppColor.primary,
+                    ),
+                    onPressed: _nextMonth,
+                  ),
+                ],
+              ),
+              Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                child: Center(
+                  child: SfCalendar(
+                    headerHeight: 0,
+                    view: CalendarView.month,
+                    controller: CalendarController(),
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
-
-
-// String getMessageDay(DateTime messageTime, bool isNewWeek) {
-//     final DateTime now = DateTime.now();
-//     final DateTime yesterday = DateTime(now.year, now.month, now.day - 1);
-
-//     if (isSameDay(messageTime, now)) {
-//       return 'Today';
-//     } else if (isSameDay(messageTime, yesterday)) {
-//       return 'Yesterday';
-//     } else if (!isNewWeek) {
-//       // Show the day name if it's a new week
-//       return DateFormat('EEEE').format(messageTime); // Requires intl package
-//     } else {
-//       // Show the date if it's not a new week
-//       return DateFormat('dd MMMM yyyy').format(messageTime); // Requires intl package
-//     }
-//   }
-
-//    Expanded(
-//                         child: ScrollConfiguration(
-//                           behavior: const ScrollBehavior().copyWith(overscroll: false),
-//                           child: ListView.builder(
-//                             physics: const ClampingScrollPhysics(),
-//                             controller: messageController,
-//                             itemCount: snapshot.data!.length,
-//                             itemBuilder: (BuildContext context, int index) {
-//                               if (index == 0 && isGroupChat && chatItem != null) {
-//                                 return Padding(
-//                                   padding: const EdgeInsets.only(top: 10, bottom: 10),
-//                                   child: Chip(
-//                                     shape: const StadiumBorder(),
-//                                     backgroundColor: Colors.grey.shade200,
-//                                     label: Padding(
-//                                       padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 2),
-//                                       child: Text(
-//                                         chatItem.groupCreatedBy!,
-//                                         style: const TextStyle(
-//                                           color: Colors.black45,
-//                                           fontSize: 12,
-//                                         ),
-//                                       ),
-//                                     ),
-//                                   ),
-//                                 );
-//                               } else {
-//                                 final messageIndex = isGroupChat ? index - 1 : index;
-//                                 final messageData = snapshot.data![messageIndex];
-//                                 final isSender = messageData.senderId == AppConst.getAccessToken();
-
-//                                 if (!isSender && !messageData.isSeen && !isGroupChat) {
-//                                   ref.read(chatControllerProvider).setChatMessageSeen(
-//                                         context,
-//                                         chatItemId,
-//                                         messageData.messageId,
-//                                         isSender,
-//                                       );
-//                                 }
-
-//                                 final bool isFirstMessageOfNewDay = messageIndex == 0 ||
-//                                     !isSameDay(
-//                                       messageData.timeSent.toDate(),
-//                                       snapshot.data![messageIndex].timeSent.toDate(),
-//                                     );
-
-//                                 final bool isNewWeek =
-//                                     messageIndex == 0 || messageData.timeSent.toDate().difference(snapshot.data![messageIndex].timeSent.toDate()).inDays >= 7;
-//                                 final String messageDay = getMessageDay(messageData.timeSent.toDate(), isNewWeek);
-
-//                                 return Column(
-//                                   crossAxisAlignment: CrossAxisAlignment.center,
-//                                   children: [
-//                                     if (isFirstMessageOfNewDay) ...[
-//                                       Padding(
-//                                         padding: const EdgeInsets.only(top: 10.0, bottom: 10),
-//                                         child: Chip(
-//                                           shape: const StadiumBorder(),
-//                                           backgroundColor: Colors.grey.shade200,
-//                                           label: Padding(
-//                                             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 2),
-//                                             child: Text(
-//                                               messageDay,
-//                                               style: const TextStyle(
-//                                                 color: Colors.black45,
-//                                                 fontSize: 12,
-//                                               ),
-//                                             ),
-//                                           ),
-//                                         ),
-//                                       ),
-//                                     ],
-//                                     MessageBox(
-//                                       message: messageData.text,
-//                                       isSender: isSender,
-//                                       data: messageData,
-//                                       isSeen: messageData.isSeen,
-//                                       messageType: messageData.type,
-//                                     ),
-//                                   ],
-//                                 );
-//                               }
-//                             },
-//                           ),
-//                         ),
-//                       ),
