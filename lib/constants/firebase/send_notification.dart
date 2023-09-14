9edb6ecc1d9ae1201aca4b937fc6ea50a1a142f3
manishchayal14 <manishchayal14@gmail.com>
2023-09-14 17:53:36 +0530

    added

--- lib/constants/firebase/send_notification.dart
+++ lib/constants/firebase/send_notification.dart
@@ -1,14 +1,24 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:yes_broker/constants/firebase/random_uid.dart';
+import 'package:yes_broker/constants/firebase/userModel/notification_model.dart';
 import 'package:yes_broker/constants/firebase/userModel/user_info.dart';
 
 import '../app_constant.dart';
 
 const serverKey = "AAAAyk5ZDg8:APA91bF9gOEKgpPLOmPskYJ5kTuxuTOxX7eRPh0XSmzPJYINEE6DjhmHQAHlU4TntXJgo92oArPrIfhSsFAEyVQEDsL7YgY0YyXBFoYgX5_i1pxoOVkQwvtSdguxzDI1n9bskHdQ-30Q";
 
-Future<void> sendNotificationTouser({required String token, required String title, required String content, required String itemid, required User currentuserdata}) async {
+Future<void> sendNotificationTouser({
+  required String token,
+  required String title,
+  required String content,
+  required String itemid,
+  required User currentuserdata,
+  required List<String> usersIds,
+}) async {
   var data = {
     'to': token,
     'notification': {'title': title, 'body': content},
@@ -21,6 +31,9 @@ Future<void> sendNotificationTouser({required String token, required String titl
   };
   await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
       body: jsonEncode(data), headers: {'Content-Type': 'application/json; charset=UTF-8', 'Authorization': 'key=$serverKey'}).then((value) {
+    if (value.statusCode == 200) {
+      setNotificationTOfirestore(title, content, itemid, currentuserdata, usersIds);
+    }
     if (kDebugMode) {
       print(value.body.toString());
     }
@@ -49,10 +62,10 @@ void notifyToUser({
       }
     }
     List<String> tokens = await User.getUserTokensByIds(userids);
-    print("tokens$tokens");
+
     for (var token in tokens) {
       if (AppConst.getFcmToken() != token) {
-        await sendNotificationTouser(token: token, title: title, content: content, itemid: itemid, currentuserdata: currentuserdata);
+        await sendNotificationTouser(token: token, title: title, content: content, itemid: itemid, currentuserdata: currentuserdata, usersIds: userids);
       }
     }
   } catch (e) {
@@ -60,4 +73,22 @@ void notifyToUser({
   }
 
   print('object');
+}
+
+Future<void> setNotificationTOfirestore(String title, String content, String itemid, User currentuserdata, List<String> usersIds) async {
+  try {
+    NotificationModel notificationModel = NotificationModel(
+      title: title,
+      notificationContent: content,
+      receiveDate: Timestamp.now(),
+      linkedItemId: itemid,
+      imageUrl: currentuserdata.image,
+      userId: usersIds.where((element) => element != AppConst.getAccessToken()).toList(),
+      isRead: false,
+      id: generateUid(),
+    );
+    await NotificationModel.addNotification(notificationModel);
+  } catch (e) {
+    print(e.toString());
+  }
 }
