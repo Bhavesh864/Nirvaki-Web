be0436257f5a973a39afca2226d26fb093a2a994
Bhavesh864 <bhaveshkhatri864@gmail.com>
2023-09-18 18:07:05 +0530

    pushclear

---
+++ lib/constants/functions/calendar/calendar_functions.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+
+import '../../../Customs/snackbar.dart';
+import '../../../riverpodstate/user_data.dart';
+import '../../firebase/calenderModel/calender_model.dart';
+import '../../firebase/random_uid.dart';
+import '../../firebase/userModel/user_info.dart';
+import '../../utils/colors.dart';
+
+Color getColorForTaskType(String taskType) {
+  // Implement logic to map task types to colors here
+  if (taskType == "Meeting") {
+    return Colors.orange; // Return the color for meetings
+  } else if (taskType == "Event") {
+    return Colors.green; // Return the color for events
+  } else if (taskType == "Appointment") {
+    return Colors.red; // Return the color for appointments
+  }
+  // You can add more cases for other task types
+  // or provide a default color if needed.
+  return AppColor.primary;
+}
+
+String toDate(DateTime dateTime) {
+  final date = DateFormat.yMMMEd().format(dateTime);
+
+  return date;
+}
+
+String toTime(DateTime dateTime) {
+  final date = DateFormat('hh:mm a').format(dateTime);
+
+  return date;
+}
+
+Future<DateTime?> pickDateTime(DateTime initialDate, {required BuildContext context, required bool pickDate, DateTime? firstDate}) async {
+  if (pickDate) {
+    final date = await showDatePicker(
+      context: context,
+      initialDate: initialDate,
+      firstDate: firstDate ?? DateTime(2015, 8),
+      lastDate: DateTime(2101),
+    );
+
+    if (date == null) return null;
+
+    final time = Duration(hours: initialDate.hour, minutes: initialDate.minute);
+
+    return date.add(time);
+  } else {
+    final timeOfDay = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(initialDate),
+    );
+
+    if (timeOfDay == null) return null;
+
+    final date = DateTime(initialDate.year, initialDate.month, initialDate.day);
+    final time = Duration(hours: timeOfDay.hour, minutes: timeOfDay.minute);
+
+    return date.add(time);
+  }
+}
+
+Future pickFromDateTime({
+  required DateTime pickedDate,
+  required bool pickDate,
+  required BuildContext context,
+  required TextEditingController dateController,
+  required TextEditingController timeController,
+}) async {
+  final date = await pickDateTime(pickedDate, pickDate: pickDate, context: context);
+
+  if (pickDate) {
+    pickedDate = date!;
+    dateController.text = toDate(date);
+  } else {
+    // pickedTime = date!;
+    timeController.text = toTime(date!);
+  }
+}
+
+void addCalenderDatatoFirebase({
+  required String title,
+  required String description,
+  required String dueDate,
+  required String time,
+  required WidgetRef ref,
+}) {
+  final User user = ref.read(userDataProvider);
+  final CalendarModel calendarModel = CalendarModel(
+    calenderTitle: title,
+    calenderDescription: description,
+    calenderType: "Meeting",
+    id: generateUid(),
+    dueDate: dueDate,
+    time: time,
+    userId: user.userId,
+    brokerId: user.brokerId,
+    managerId: user.managerid,
+  );
+
+  CalendarModel.addCalendarModel(calendarModel);
+}
+
+void onAddTask({
+  required WidgetRef ref,
+  required BuildContext context,
+  required TextEditingController dateController,
+  required TextEditingController timeController,
+  required TextEditingController titleController,
+  required TextEditingController descriptionController,
+}) {
+  addCalenderDatatoFirebase(
+    title: titleController.text.trim(),
+    description: descriptionController.text.trim(),
+    dueDate: dateController.text.trim(),
+    time: timeController.text.trim(),
+    ref: ref,
+  );
+  titleController.clear();
+  descriptionController.clear();
+  dateController.clear();
+  timeController.clear();
+  customSnackBar(context: context, text: 'Item added successfully!');
+}
+
+void onDeleteTask({
+  required BuildContext context,
+  required TextEditingController dateController,
+  required TextEditingController timeController,
+  required TextEditingController titleController,
+  required TextEditingController descriptionController,
+  required CalendarModel calendarModel,
+}) {
+  CalendarModel.deleteCalendarModel(calendarModel.id!);
+  titleController.clear();
+  descriptionController.clear();
+  dateController.clear();
+  timeController.clear();
+
+  customSnackBar(context: context, text: 'Item deleted successfully!');
+}
+
+void onUpdateTask({
+  required BuildContext context,
+  required TextEditingController dateController,
+  required TextEditingController timeController,
+  required TextEditingController titleController,
+  required TextEditingController descriptionController,
+  required CalendarModel calendarModel,
+}) {
+  final CalendarModel updatedModel = CalendarModel(
+    id: calendarModel.id,
+    calenderType: 'Meeting',
+    brokerId: calendarModel.brokerId,
+    userId: calendarModel.userId,
+    managerId: calendarModel.managerId,
+    calenderTitle: titleController.text,
+    calenderDescription: descriptionController.text,
+    dueDate: dateController.text,
+    time: timeController.text,
+  );
+  CalendarModel.updateCalendarModel(updatedModel);
+  titleController.clear();
+  descriptionController.clear();
+  dateController.clear();
+  timeController.clear();
+  customSnackBar(context: context, text: 'Item updated successfully!');
+}
--- lib/screens/main_screens/caledar_screen.dart
+++ lib/screens/main_screens/caledar_screen.dart
@@ -3,42 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:yes_broker/Customs/custom_text.dart';
 
-import 'package:yes_broker/constants/firebase/calenderModel/calender_model.dart';
-import 'package:yes_broker/constants/firebase/random_uid.dart';
 import 'package:yes_broker/constants/functions/workitems_detail_methods.dart';
 import 'package:yes_broker/constants/utils/colors.dart';
 import 'package:yes_broker/customs/custom_fields.dart';
 import 'package:yes_broker/customs/loader.dart';
+import 'package:yes_broker/customs/responsive.dart';
 import 'package:yes_broker/riverpodstate/user_data.dart';
 
+import '../../constants/firebase/calenderModel/calender_model.dart';
 import '../../constants/firebase/userModel/user_info.dart';
-
-class EventDataSource extends CalendarDataSource {
-  EventDataSource(List<CalenderModel> appointments) {
-    this.appointments = appointments;
-  }
-
-  getCalendarEvent(int index) => appointments![index] as CalenderModel;
-
-  @override
-  DateTime getStartTime(int index) {
-    DateFormat inputFormat = DateFormat('E, MMM d, y');
-    return inputFormat.parse(getCalendarEvent(index).dueDate);
-  }
-
-  // @override
-  // DateTime getEndTime(int index) {
-  //   DateFormat inputFormat = DateFormat('E, MMM d, y');
-  //   return inputFormat.parse(getCalendarEvent(index).dueDate).add(const Duration(hours: 3));
-  // }
-
-  @override
-  String getSubject(int index) => getCalendarEvent(index).calenderTitle;
-
-  @override
-  Color getColor(int index) => AppColor.primary.withOpacity(0.5);
-}
+import '../../constants/functions/calendar/calendar_functions.dart';
+import '../../widgets/calendar/event_data.dart';
 
 class CalendarScreen extends ConsumerStatefulWidget {
   const CalendarScreen({super.key});
@@ -49,22 +26,13 @@ class CalendarScreen extends ConsumerStatefulWidget {
 
 class _CalendarScreenState extends ConsumerState<CalendarScreen> {
   late DateTime _displayedMonth;
-  final TextEditingController titleController = TextEditingController();
-  final TextEditingController dateController = TextEditingController();
-  final TextEditingController timeController = TextEditingController();
-  final TextEditingController descriptionController = TextEditingController();
   DateTime selectedDate = DateTime.now();
+  Stream<QuerySnapshot<Map<String, dynamic>>>? stream;
 
   @override
   void initState() {
     super.initState();
     _displayedMonth = DateTime.now();
-  }
-
-  @override
-  void dispose() {
-    titleController.dispose();
-    super.dispose();
   }
 
   void _previousMonth() {
@@ -79,168 +47,168 @@ class _CalendarScreenState extends ConsumerState<CalendarScreen> {
     });
   }
 
-  void addCalenderDatatoFirebase({
-    required String title,
-    required String description,
-    required String dueDate,
-    required String time,
-  }) {
-    final User user = ref.read(userDataProvider);
-    final CalenderModel calenderModel = CalenderModel(
-      calenderTitle: title,
-      calenderDescription: description,
-      calenderType: "Meeting",
-      id: generateUid(),
-      dueDate: dueDate,
-      time: time,
-      userId: user.userId,
-      brokerId: user.brokerId,
-      managerId: user.managerid,
-    );
-
-    CalenderModel.addCalenderModel(calenderModel);
-  }
-
   @override
   Widget build(BuildContext context) {
     final User user = ref.read(userDataProvider);
 
-    return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('calenderDetails').where('brokerId', isEqualTo: user.brokerId).snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Loader();
-          }
-          if (snapshot.hasData) {
-            final dataList = snapshot.data!.docs;
-            List<CalenderModel> calenderList = dataList.map((doc) => CalenderModel.fromSnapshot(doc)).toList();
-            final List<CalenderModel> selectedEvents = calenderList;
-            return Row(
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: SfCalendar(
-                      // headerHeight: 0,
-                      dataSource: EventDataSource(selectedEvents),
-                      view: CalendarView.day,
-                      allowedViews: const [
-                        CalendarView.day,
-                        CalendarView.week,
-                      ],
-                      showDatePickerButton: true,
-                      timeSlotViewSettings: const TimeSlotViewSettings(startHour: 8, endHour: 24),
-                      initialDisplayDate: selectedDate,
-                      appointmentBuilder: (context, calendarAppointmentDetails) {
-                        final event = calendarAppointmentDetails.appointments.first;
+    return Scaffold(
+      appBar: Responsive.isMobile(context)
+          ? AppBar(
+              iconTheme: const IconThemeData(
+                color: Colors.black,
+              ),
+              title: const CustomText(
+                title: 'Calendar',
+              ),
+            )
+          : null,
+      body: SafeArea(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('calenderDetails').where('brokerId', isEqualTo: user.brokerId).snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Loader();
+            }
+            if (snapshot.hasData) {
+              final dataList = snapshot.data!.docs;
+              List<CalendarModel> calenderList = dataList.map((doc) => CalendarModel.fromSnapshot(doc)).toList();
 
-                        return Container(
-                          height: calendarAppointmentDetails.bounds.height,
-                          width: calendarAppointmentDetails.bounds.width,
-                          decoration: BoxDecoration(
-                            color: AppColor.primary.withOpacity(0.5),
-                            borderRadius: BorderRadius.circular(12),
+              return Row(
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: SfCalendar(
+                        showCurrentTimeIndicator: false,
+                        dataSource: EventDataSource(calenderList),
+                        view: CalendarView.day,
+                        showDatePickerButton: true,
+                        timeSlotViewSettings: const TimeSlotViewSettings(startHour: 8, endHour: 24),
+                        initialDisplayDate: selectedDate,
+                        showTodayButton: true,
+                        showNavigationArrow: true,
+                        backgroundColor: Colors.white,
+                        allowedViews: const [
+                          CalendarView.day,
+                          CalendarView.week,
+                        ],
+                        onTap: (details) {
+                          if (details.appointments == null) return;
+                          final CalendarModel event = details.appointments!.first;
+                          showAddCalendarModal(
+                            context: context,
+                            isEdit: true,
+                            calendarModel: event,
+                            ref: ref,
+                          );
+                        },
+                        appointmentBuilder: (context, calendarAppointmentDetails) {
+                          final event = calendarAppointmentDetails.appointments.first;
+
+                          return Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 10),
+                            height: calendarAppointmentDetails.bounds.height,
+                            width: calendarAppointmentDetails.bounds.width,
+                            decoration: BoxDecoration(
+                              color: getColorForTaskType('').withOpacity(0.1),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Center(
+                              child: Text(
+                                event.calenderTitle,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  if (!Responsive.isMobile(context)) ...[
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                      width: 1,
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_back_ios,
+                                  color: AppColor.primary,
+                                ),
+                                onPressed: _previousMonth,
+                              ),
+                              Text(
+                                DateFormat.yMMMM().format(_displayedMonth),
+                                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: AppColor.primary,
+                                ),
+                                onPressed: _nextMonth,
+                              ),
+                            ],
                           ),
-                          child: Center(
-                            child: Text(
-                              event.calenderTitle,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+                          SizedBox(
+                            width: 240,
+                            child: Center(
+                              child: SfCalendar(
+                                onTap: (calendarLongPressDetails) {
+                                  selectedDate = calendarLongPressDetails.date!;
+                                  setState(() {});
+                                },
+                                headerHeight: 0,
+                                initialDisplayDate: selectedDate,
+                                initialSelectedDate: selectedDate,
+                                dataSource: EventDataSource(calenderList),
+                                cellBorderColor: Colors.transparent,
+                                view: CalendarView.month,
+                                controller: CalendarController(),
+                                backgroundColor: Colors.white,
                               ),
                             ),
                           ),
-                        );
-                      },
-                      showTodayButton: true,
-                      showNavigationArrow: true,
-                      backgroundColor: Colors.white,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                  width: 1,
-                  color: Colors.grey.withOpacity(0.5),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            icon: const Icon(
-                              Icons.arrow_back_ios,
-                              color: AppColor.primary,
-                            ),
-                            onPressed: _previousMonth,
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Divider(),
                           ),
-                          Text(
-                            DateFormat.yMMMM().format(_displayedMonth),
-                            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          IconButton(
-                            icon: const Icon(
-                              Icons.arrow_forward_ios,
-                              color: AppColor.primary,
-                            ),
-                            onPressed: _nextMonth,
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 240,
-                        child: Center(
-                          child: SfCalendar(
-                            onLongPress: (calendarLongPressDetails) {
-                              selectedDate = calendarLongPressDetails.date!;
-                            },
-                            headerHeight: 0,
-                            dataSource: EventDataSource(calenderList),
-                            cellBorderColor: Colors.transparent,
-                            view: CalendarView.month,
-                            controller: CalendarController(),
-                            backgroundColor: Colors.white,
-                          ),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Divider(),
-                      ),
-                      CustomButton(
-                        text: 'Create new',
-                        onPressed: () {
-                          showAddCalendarModal(
-                            context,
-                            titleController,
-                            dateController,
-                            timeController,
-                            descriptionController,
-                            () {
-                              addCalenderDatatoFirebase(
-                                title: titleController.text.trim(),
-                                description: descriptionController.text.trim(),
-                                dueDate: dateController.text.trim(),
-                                time: timeController.text.trim(),
+                          CustomButton(
+                            height: 40,
+                            width: 160,
+                            text: 'Create new +',
+                            onPressed: () {
+                              showAddCalendarModal(
+                                context: context,
+                                isEdit: false,
+                                ref: ref,
                               );
                             },
-                          );
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            );
-          }
-          return const SizedBox.shrink();
-        });
+                          )
+                        ],
+                      ),
+                    ),
+                  ]
+                ],
+              );
+            }
+            return const SizedBox.shrink();
+          },
+        ),
+      ),
+    );
   }
 }
