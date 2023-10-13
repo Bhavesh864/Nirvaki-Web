d7070e3bc19f92ac052b16bea0a157dddf62a79b
manishchayal14 <manishchayal14@gmail.com>
2023-10-13 18:11:18 +0530

    ad

--- lib/screens/main_screens/home_screen.dart
+++ lib/screens/main_screens/home_screen.dart
@@ -39,7 +39,6 @@ class HomeScreenState extends ConsumerState<HomeScreen> {
 
   @override
   void initState() {
-    print("home");
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (Beamer.of(context).currentBeamLocation.state.routeInformation.location != '/profile') {
