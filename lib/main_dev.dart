90292c8e8d22daa314019e74f7205e681053561a
manishchayal14 <manishchayal14@gmail.com>
2023-10-18 18:00:55 +0530

    addd

--- lib/main_dev.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:yes_broker/env/app_env.dart';
-import 'package:yes_broker/main.dart';
-
-void main() {
-  AppEnvironment.setupEnv(Environment.dev);
-  runApp(const MyApp());
-}
