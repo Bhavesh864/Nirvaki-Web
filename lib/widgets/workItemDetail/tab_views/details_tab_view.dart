820059cd9c4b3ad0ca4d764ca96f02c415dec910
manishchayal14 <manishchayal14@gmail.com>
2023-10-20 17:54:24 +0530

    ad

--- lib/widgets/workItemDetail/tab_views/details_tab_view.dart
+++ lib/widgets/workItemDetail/tab_views/details_tab_view.dart
@@ -850,17 +850,6 @@ class _DetailsTabViewState extends State<DetailsTabView> {
             //       ).toList() ??
             //       [SizedBox()],
             // )
-
-            // MapViewWidget(
-            //   latLng: widget.isLeadView
-            //       ? LatLng(widget.data.preferredlocation![0], widget.data.preferredlocation![1])
-            //       : LatLng(widget.data.propertylocation[0], widget.data.propertylocation[1]),
-            //   state: widget.data.preferredlocality!.state!,
-            //   city: widget.data.preferredlocality!.city!,
-            //   addressline1: widget.data.preferredlocality?.addressline1,
-            //   addressline2: widget.data.preferredlocality?.addressline2,
-            //   locality: widget.data.preferredlocality!.locality!,
-            // ),
           ] else ...[
             MapViewWidget(
               latLng: widget.isLeadView
