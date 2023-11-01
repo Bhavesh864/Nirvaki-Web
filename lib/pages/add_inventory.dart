f10721777bc6817a6e0bc25f5310f313bf9b1992
madhavsewag04 <madhavsewag04@gmail.com>
2023-11-01 17:12:17 +0530

    lead change

--- lib/pages/add_inventory.dart
+++ lib/pages/add_inventory.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -81,7 +83,8 @@ class AddInventoryState extends ConsumerState<AddInventory> {
   }
 
   nextQuestion({List<Screen>? screensDataList, required String option}) {
-    if (currentScreenIndex == 3) {
+    int numberIndex = isEdit ? 2 : 3;
+    if (currentScreenIndex == numberIndex) {
       final List<Map<String, dynamic>> selectedValues = ref.read(myArrayProvider);
       final mobileNoValue = selectedValues.where((e) => e["id"] == 7).toList();
       final whatsappNoValue = selectedValues.where((e) => e["id"] == 8).toList();
--- lib/pages/add_lead.dart
+++ lib/pages/add_lead.dart
@@ -86,7 +86,8 @@ class _AddLeadState extends ConsumerState<AddLead> {
   }
 
   nextQuestion({List<Screen>? screensDataList, required String option}) {
-    if (currentScreenIndex == 3) {
+    int numberIndex = isEdit ? 2 : 3;
+    if (currentScreenIndex == numberIndex) {
       final List<Map<String, dynamic>> selectedValues = ref.read(myArrayProvider);
       final mobileNoValue = selectedValues.where((e) => e["id"] == 7).toList();
       final whatsappNoValue = selectedValues.where((e) => e["id"] == 8).toList();
