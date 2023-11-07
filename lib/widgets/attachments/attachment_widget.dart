908800613f915b075b7ef09ec1eca392b39abe08
manishchayal14 <manishchayal14@gmail.com>
2023-11-07 15:24:57 +0530

    ad

--- lib/widgets/attachments/attachment_widget.dart
+++ lib/widgets/attachments/attachment_widget.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 // import 'dart:html';
 
-import 'dart:developer';
-
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_file_downloader/flutter_file_downloader.dart';
